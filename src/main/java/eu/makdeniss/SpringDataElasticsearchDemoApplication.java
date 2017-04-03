bef59e0d254c8c6f42f0ab60fc02a3797983157d
makdeniss@gmail.com <020286Deniss>
2017-04-03 18:00:58 +0300

    Initial commit

---
+++ src/main/java/eu/makdeniss/SpringDataElasticsearchDemoApplication.java
@@ -0,0 +1,12 @@
+package eu.makdeniss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticsearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataElasticsearchDemoApplication.class, args);
+	}
+}
